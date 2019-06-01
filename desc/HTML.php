1fbe74174f9d49082e7eaf235bbeb966a1a85e42
seungukson <seunguksondev@gmail.com>
2019-06-01 16:35:04 +0900

    all files are updated

---
+++ desc/HTML.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>asdf</title>
+  </head>
+  <body>
+    <?php
+    echo 10;
+    adfdasf
+    is Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ali
+    ?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,31 +1,41 @@
+<?php
+function printDesc()
+{
+  if(empty($_GET["name"]))
+  {
+    echo file_get_contents("desc/welcome");
+  }
+  else {
+    $pathof_name = "desc/".$_GET["name"];
+    echo file_get_contents($pathof_name);
+  }
+}
+function printList()
+{
+  $desc_list = scandir('./desc');
+  for($i = 0; $i < sizeof($desc_list);++$i)
+  {
+    if($desc_list[$i]=='.' || $desc_list[$i]=='..') continue;
+    echo "<li><a href = \"index.php?name=$desc_list[$i]\"> $desc_list[$i]</a></li>\n";
+  }
+}
+?>
+
 <!doctype html>
 <html>
-<head>
-<title>WEB1 - Welcome</title>
-<meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="index.html">WEB</a></h1>
-<ol>
-  <li><a href="index.php?id=HTML"> HTML</a></li>
-  <li><a href="index.php?id=CSS"> CSS</a></li>
-  <li><a href="index.php?id=Java"> JavaScript</a></li>
-  <li><a href="index.php?id=PHP"> PHP</a></li>
-</ol>
-<p><a href="https://www.w3.org/TR/html53/" target="_blank" title="specification으로이동">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.<p/>
-
-<h1> <?php echo $_GET['id']; ?>란 무엇인가?</h1>
-<p>PHP: Hypertext Preprocessor is a general-purpose programming language originally designed for web development. It was originally created by Rasmus Lerdorf in 1994; the PHP reference implementation is now produced by The PHP Group</p>
-
-그럼 우리 함께 파이썬 탐험을 떠나 볼까요~<br>
-<img src="coding.jpg" width = 100%>
-<h1 class="logo"><a tabindex="2" accesskey="1" href="/"><img src="C:\Users/ssson/Pictures/cgv_mark.png" width="90" height="53" alt="W3C" /></a> <span class="alt-logo">W3C</span></h1>
-<a href="https://www.w3schools.com">Visit W3Schools.com!</a>
-<h1>This is heading 1</h1>
-<h2>This is heading 2</h2>
-<h3>This is heading 3</h3>
-<h4>This is heading 4</h4>
-<h5>This is heading 5</h5>
-<h6>This is heading 6</h6>
-</body>
+  <head>
+    <title>Web-1</title>
+    <meta charset = "utf-8">
+  </head>
+  <body>
+    <h1><a href="index.php">WEB</a></h1>
+    <ol>
+      <?php
+      printList()
+      ?>
+    </ol>
+    <?php
+      printDesc()
+     ?>
+  </body>
 </html>
